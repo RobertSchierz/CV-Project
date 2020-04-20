734b2dec6a14b5c882416769070478ff13df3681
RobertSchierz <36508029+RobertSchierz@users.noreply.github.com>
2020-04-20 16:54:21 +0200

    Ich UI + Globe Animation

---
+++ Assets/Own Scripts/Ichuicontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ichuicontroller : MonoBehaviour
+{
+
+    public GameObject earth;
+    public Animator earthanimator;
+    
+    public void startauslandsanimation()
+    {
+        if (earth != null)
+        {
+            if (earthanimator != null)
+            {
+                earthanimator.SetBool("startearth", true);
+            }
+            earth.GetComponent<MeshRenderer>().enabled = true;
+            LeanTween.moveLocalY(earth, 1, 2).setEaseInOutBack();
+        }
+    }
+}
